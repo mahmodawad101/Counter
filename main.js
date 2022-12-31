6252d10f1d69ebf3a8390ec8e4744688585ac7e3
Mahmoud Awad <medo39286@gmail.com>
2023-01-01 01:27:10 +0200

    ..

---
+++ main.js
@@ -0,0 +1,22 @@
+
+    let daysDiv = document.querySelector(".days .dayss")
+    let hoursDiv = document.querySelector(".hours .hourss")
+    let minutesDiv = document.querySelector(".minutes .minutess")
+    let secondsDiv = document.querySelector(".seconds .secondss")
+    
+    let fDate = new Date("10/01/2023");
+    let sDate = new Date();
+    let dInTime = fDate.getTime() - sDate.getTime();
+
+
+    setInterval(function(){
+        dInTime -= 1000;
+    },1000)
+    let inter = setInterval(()=> {
+        daysDiv.innerHTML = ` ${Math.round(dInTime / (1000 * 3600 * 24))}`
+        hoursDiv.innerHTML = ` ${Math.round(dInTime / (1000 * 3600))}`
+        minutesDiv.innerHTML = ` ${Math.round(dInTime / (1000 * 60))}`
+        secondsDiv.innerHTML = ` ${Math.round(dInTime / 1000  )}`
+      },1000);
+      
+      // create setInterval function ?
